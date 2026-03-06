cd3cbc812417e12e05b80bd0a96b4be018d5602d
isaac-ndungu <isaacndungu478@gmail.com>
2026-03-06 15:57:52 +0300

    implement dynamic routing to handle page navigation and content loading. update templates for about.html, parks.html and index.html

--- js/script.js
+++ js/script.js
@@ -1,6 +1,5 @@
 // DOM Elements
-
-const cardContainer = document.getElementById('cardContainer');
+const content = document.getElementById('content');
 
 // fetch
 
@@ -23,6 +22,10 @@ async function fetchParksData() {
 // create park card
 
 function createParkCards() {
+    // clear existing cards
+    const cardContainer = document.getElementById('cardContainer');
+    cardContainer.innerHTML = '';
+
     parksData.forEach(park => {
         const card = document.createElement('div');
         card.classList.add('park-card');
@@ -51,11 +54,73 @@ document.addEventListener('DOMContentLoaded', async () => {
 
 
 //  Dynamic routing
-
+const pageTitle = "safarii"
 
 const routes = {
     404: {
         template: "/templates/404.html",
+        title: "404 | " + pageTitle,
+        description: "Page not found"
+    },
+    "/": {
+        template: "/templates/index.html",
+        title: "Home " + pageTitle,
+        description: "Safarii home page"
+    },
+    about: {
+        template: "/templates/about.html",
+        title: "About Us | " + pageTitle,
+        description: "About Safarii "
+    },
+    parks: {
+        template: "/templates/parks.html",
+        title: "Parks | " + pageTitle,
+        description: "Featured parks"
+    },
+    safari: {
+        template: "/template/safari.com",
+        title: "Safaris | " + pageTitle,
+        description: "Featured safari packages"
+    },
+    contact: {
+        template: "/templates/contact.html",
+        title: "Contact Us |" + pageTitle,
+        description: "Contact Us"
+    },
+    blog: {
+        template: "/templates.blog.html",
+        title: "Blog | " + pageTitle,
+        description: " Safarii blogs"
+    },
+    nairobi: {
+        template: "../template/nairobi-park.html",
+        title: "Nairobi National Park | " + pageTitle,
+        description: "Nairobi national park pahe"
+    },
+};
 
+// location handling
+const locationHandler = async () => {
+    var location = window.location.hash.replace('#', '');
+
+    if (location.length == 0){
+        location = '/';
     }
+
+    // get the route object from the routes object
+    const route = routes[location] || routes[404];
+    
+    // get the html from the template
+    html = await fetch(route.template).then((response) => response.text());
+
+    content.innerHTML = html;
+    document.title = route.title;
+
+    // set the desctiprion of the document to the descriotion of the route
+    document.querySelector('meta[name="description"]').setAttribute("content", route.description);
+        createParkCards();
 }
+
+window.addEventListener("hashchange", locationHandler);
+
+locationHandler();
